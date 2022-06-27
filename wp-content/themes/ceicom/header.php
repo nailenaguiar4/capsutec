20e40b57b6a1b73eae023abb2b2339cfdb47f9af
Diego Granados <diego@codeando.edu.pe>
2022-06-27 08:44:46 +0200

    Maquetación del Header
    Falta terminar funcionalidades con JS y Wordpress

--- wp-content/themes/ceicom/header.php
+++ wp-content/themes/ceicom/header.php
@@ -11,6 +11,27 @@
     <title>Capsutec</title>
 </head>
 <body>
-    <header>
         <!-- Cabecera/Header del Sitio -->
+    <div class="select-language-header">
+        <div class="select-language-container">
+            <img src="<?php echo get_theme_file_uri() . '/assets/img/L-icon.png' ?>" alt="" class="icon">
+            <a href="#"><img src="<?php echo get_theme_file_uri() . '/assets/img/en-icon.png' ?>" alt=""></a>
+            <a href="#"><img src="<?php echo get_theme_file_uri() . '/assets/img/sp-icon.png' ?>" alt=""></a>
+            <a href="#"><img src="<?php echo get_theme_file_uri() . '/assets/img/br-icon.png' ?>" alt=""></a>
+        </div>
+    </div>
+    <header class="head_container">
+            <div class="logo">
+                <img src="<?php echo get_theme_file_uri() . '/assets/img/Logo.png'  ?>">
+            </div>
+            <nav class="menu" id="myTopnav">
+                <ul>
+                    <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="openNav()">&#9776;</a>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">Quem Somos</a></li>
+                    <li><a href="#">Produtos</a></li>
+                    <li><a href="#">Orçamento</a></li>
+                    <li><a href="#">Contato</a></li>
+                </ul>
+            </nav>
     </header>
